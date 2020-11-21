46afefcef5ef8b432812c43a9d0ed4e9876aceb0
Abel Masila <abelmasila@gmail.com>
2020-11-21 18:14:59 +0300

    webback config for ts done

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,17 @@
 const path = require("path");
 module.exports = {
   entry: "./src/index.ts",
-  out: {
+  output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "ts-loader",
+        include: [path.resolve(__dirname, "src")],
+      },
+    ],
   },
 };
