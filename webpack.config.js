2b2eb6c5f444bd3a3ca5f860ad22d9388c2cf9a0
Abel Masila <abelmasila@gmail.com>
2020-11-21 18:26:11 +0300

    better webpack dev server workflow

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "public"),
     filename: "bundle.js",
+    publicPath: "public",
+  },
+  devServer: {
+    contentBase: "./public",
+    hot: true,
   },
   module: {
     rules: [
